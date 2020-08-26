f906518773d50f5cb63a5579a407d95f0812cfb0
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-08-26 09:18:03 +0900

    いいねボタンcomponent作成

--- js/main.js
+++ js/main.js
@@ -1,50 +1,24 @@
 (function() {
   'use strict';
 
-  var vm = new Vue({
-    el: '#app',
-    data: {
-      newItem: '',
-      todos: []
-    },
-    watch: {
-      todos: {
-        handler: function() {
-          localStorage.setItem('todos', JSON.stringify(this.todos));
-        },
-        deep: true
+  var likeComponent = Vue.extend({
+    data: function() {
+      return {
+        count: 0
       }
     },
-    mounted: function() {
-      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-    },
+    template: '<button @click="countUp">Like {{ count }}</button>',
     methods: {
-      addItem: function() {
-        var item = {
-          title: this.newItem,
-          isDone: false
-        };
-        this.todos.push(item);
-        this.newItem = '';
-      },
-      deleteItem: function(index) {
-        if (confirm('are you sure?')) {
-          this.todos.splice(index, 1);
-        }
-      },
-      purge: function(index) {
-        if (!confirm('delete finished?')) {
-          return;
-        }
-        this.todos = this.remaining;
+      countUp: function() {
+        this.count++;
       }
-    },
-    computed: {
-      remaining: function() {
-        return this.todos.filter(function(todo) {
-          return !todo.isDone;
-        });
-      }
+    }
+  });
+
+  var app = new Vue({
+    el: '#app',
+    components: {
+      'like-component': likeComponent
     }
   });
 })();
